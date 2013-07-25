a5fc7917b68de4ab3f3ad853842bdf821dad0dea
Jan Krueger <jan.krueger@gmail.com>
2013-07-25 18:09:42 +0200

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject contentjon.d3 "0.1.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :hooks           [leiningen.cljsbuild]
+  :plugins         [[lein-cljsbuild "0.3.2"]]
+  :dependencies    [[org.clojure/clojure "1.5.1"]])
+
